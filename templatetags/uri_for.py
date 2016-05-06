7dec19d668d6db24e081090bf357ece1f856537f
Tom van der Kleij <aajvdkleij@gmail.com>
2016-05-06 21:06:22 +0200

    Cleaned up uri_for.py: removed unused imports, simplified code

--- templatetags/uri_for.py
+++ templatetags/uri_for.py
@@ -1,4 +1,3 @@
-import logging
 import webapp2
 
 from google.appengine.ext import webapp
@@ -7,5 +6,7 @@ register = webapp.template.create_template_register()
 
 @register.simple_tag
 def uri_for(route, param1=None, param2=None, param3=None):
-	args = filter(lambda param: param != None, [param1, param2, param3])
-	return webapp2.uri_for(route, None, *args)
+	# The Django version used by App Engine does not support *args or **kwargs
+	# in its template engine. Hence the poor man's *args solution with 
+	# a maximum of 3 arguments.
+	return webapp2.uri_for(route, None, *[param1, param2, param3])
